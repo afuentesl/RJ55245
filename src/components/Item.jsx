d03404c39d9bf316a7d5febcd1ce6910aa1ae82e
afuentesl <alejandro@rflex.cl>
2023-08-06 18:02:48 -0400

    Added Item component.

---
+++ src/components/Item.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Item = (props) => {
+	const { item } = props;
+	return (
+		<div key={item.id} className="col-span-1 flex flex-col bg-white border-2 p-4 rounded-lg ">
+			<img className="rounded-lg mb-3 border-black border max-h-30 max-w-2" src={item.img} />
+			<h2 className="mb-2 font-bold text-2xl text-center">
+				{item.name}
+			</h2>
+			<p className="text-md text-justify">{item.description}</p>
+			<div className="flex flex-wrap mt-auto pt-3 text-xs">
+				<p className="mr-2 mb-2 font-bold text-xl">Precio: ${item.price}</p>
+			</div>
+
+			<div className="flex flex-col items-center">
+				<Link to={`/item/${item.id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded align-middle">Ver detalle</Link>
+			</div>
+		</div>
+	)
+}
+
+export default Item;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,8 @@
-import { Link, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../firebase/config"
+import Item from "./Item"
 
 const ItemListContainer = () => {
 	const [products, setProducts] = useState([])
@@ -13,7 +14,6 @@ const ItemListContainer = () => {
 		setLoading(true)
 
 		const productosRef = collection(db, 'productos')
-
 		const q = categoryId ? query(productosRef, where('categoryName', '==', categoryId)) :
 			productosRef
 
@@ -44,20 +44,7 @@ const ItemListContainer = () => {
 							<div className="mx-auto p-6 grid grid-cols-4 gap-4  bg-blue-950" >
 								{
 									products.map((product) => {
-										return <div key={product.id} className="col-span-1 flex flex-col bg-white border-2 p-4 rounded-lg ">
-											<img className="rounded-lg mb-3 border-black border max-h-30 max-w-2" src={product.img} />
-											<h2 className="mb-2 font-bold text-2xl text-center">
-												{product.name}
-											</h2>
-											<p className="text-md text-justify">{product.description}</p>
-											<div className="flex flex-wrap mt-auto pt-3 text-xs">
-												<p className="mr-2 mb-2 font-bold text-xl">Precio: ${product.price}</p>
-											</div>
-
-											<div className="flex flex-col items-center">
-												<Link to={`/item/${product.id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded align-middle">Ver detalle</Link>
-											</div>
-										</div>
+										return <Item key={product.id} item={product}></Item>
 
 									})}
 							</div>
